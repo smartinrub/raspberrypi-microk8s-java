11b63cfaa291c1b4c32cfcedb2e2ae8f8a27da81
Sergio Martin Rubio <econsergio@gmail.com>
2020-04-13 14:01:39 +0100

    Clean up

--- src/main/java/com/sergiomartinrubio/raspberrypimicrok8sjava/Application.java
+++ src/main/java/com/sergiomartinrubio/raspberrypimicrok8sjava/Application.java
@@ -1,24 +1,25 @@
 package com.sergiomartinrubio.raspberrypimicrok8sjava;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @RestController
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @GetMapping("/uppercase/{input}")
-    public String uppercase(@PathVariable("input") String input) {
-        log.info("Converting string to uppercase...");
-        return input.toUpperCase();
-    }
+	@GetMapping("/uppercase/{inut}")
+	public String uppercase(@PathVariable("input") String input) {
+		log.info("Converting string to uppercase...");
+		return input.toUpperCase();
+	}
 }
